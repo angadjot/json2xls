2f694e477b84051d0eb5c1af44af8934edbac877
Angadjot Singh Bhasin <asbhasin123@gmail.com>
2020-08-27 15:08:23 +0530

    Add ability to Process multiple sheets

--- example/index.js
+++ example/index.js
@@ -2,7 +2,11 @@ var json2xls = require('../lib/json2xls');
 var data = require('../spec/arrayData.json');
 var fs = require('fs');
 
-var xls = json2xls(data,{});
+// Single Sheet
+var xls = json2xls(data, {});
+fs.writeFileSync('output.xlsx', xls, 'binary');
 
-
-fs.writeFileSync('output.xlsx',xls, 'binary');
+// Multi Sheet
+var interviewData = require('../spec/interview.json');
+var interviewXls = json2xls(interviewData, {});
+fs.writeFileSync('interview.xlsx', interviewXls, 'binary');
